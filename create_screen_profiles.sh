08857816e202e766b8cf83c01f79c769c00a0ff0
Aaron Bernstein <abernstein@godaddy.com>
2013-12-03 17:25:39 -0700

    Added filtering and the output from two calls

--- create_screen_profiles.sh
+++ create_screen_profiles.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+#
+# Currently this takes two params:
+# type && env = gives two filters levels
+#
+#
 output="source /home/$(whoami)/.screenrc\n"
 hostlist="./host_list"
 type=$1
 env=$2
+
+configfile="./.screenrc-${type}${env}";
 
 i=0;
 for host in `cat ${hostlist} | grep -i "$type" | grep -i "$env"`; 
@@ -10,4 +17,5 @@ do
   shortname=`echo $host | cut -d'.' -f1`
   output+="screen -t ${shortname} $((i++)) ssh $(whoami)@${host};\n"
 done
-echo -e $output;
+echo -e "To execute screen -c ${configfile}:\n${output}";
+echo -e $output > $configfile;
