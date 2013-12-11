e166094d18025e9e1c7cf88443e93f24f99b6aa4
Aaron Bernstein <abernstein@godaddy.com>
2013-12-11 11:17:47 -0700

    Added usage doc

--- create_screen_profiles.sh
+++ create_screen_profiles.sh
@@ -4,18 +4,25 @@
 # type && env = gives two filters levels
 #
 #
-output="source /home/$(whoami)/.screenrc\n"
-hostlist="./host_list"
-type=$1
-env=$2
+if [ ${#@} == 0 ]; then
+  usage="Usage: $(basename $BASH_SOURCE) [param1] [param2]"
+  usage+="\n* param1: type of server"
+  usage+="\n* param2: environment\n"
+  echo -e $usage;
+else
+  output="source /home/$(whoami)/.screenrc\n"
+  hostlist="./host_list"
+  type=$1
+  env=$2
 
-configfile="./.screenrc-${type}${env}";
+  configfile="./.screenrc-${type}${env}";
 
-i=0;
-for host in `cat ${hostlist} | grep -i "$type" | grep -i "$env"`; 
-do
-  shortname=`echo $host | cut -d'.' -f1`
-  output+="screen -t ${shortname} $((i++)) ssh $(whoami)@${host}\n"
-done
-echo -e "To execute screen -c ${configfile}:\n${output}";
-echo -e $output > $configfile;
+  i=0;
+  for host in `cat ${hostlist} | grep -i "$type" | grep -i "$env"`;
+  do
+    shortname=`echo $host | cut -d'.' -f1`
+    output+="screen -t ${shortname} $((i++)) ssh $(whoami)@${host}\n"
+  done
+  echo -e "To execute screen -c ${configfile}:\n${output}";
+  echo -e $output > $configfile;
+fi;
